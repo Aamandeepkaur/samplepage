097deb6685e814124c15b6c4c06026bf44119f86
Aamandeepkaur <60448754+Aamandeepkaur@users.noreply.github.com>
2020-04-01 11:51:07 -0400

    Add files via upload

---
+++ samplepage.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>The coffee house</title>
+<link rel="stylesheet"  href="javajam.css" media="print">
+</head>
+<body>
+<div id="wrapper">
+<header>
+    <h1>My Sample Web Page</h1>
+    
+    
+</header>
+<nav>
+<a href="home.html">Home</a> &nbsp; 
+<a href="products.html">Products</a> &nbsp; 
+<a href="about.html">About Us</a> &nbsp; 
+<a href="contact.html">Contact Us</a>
+</nav>
+<main>
+
+
+<div>
+54321 Route 42<br>
+Ellison Bay, WI 54210<br>
+888-555-5555<br><br>
+</div> 
+</main>
+<footer>
+Copyright &copy; FinalProject<br>
+<a href="mailto:Amandeep@kaur.com">Amandeep@Kaur.com</a>
+</footer>
+</div>
+</body>
+</html>
